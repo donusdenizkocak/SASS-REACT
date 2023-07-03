afbfec272e5a37334fddd14b1063a645a1cde28a
denizderK <denizderya8668@gmail.com>
2023-07-03 22:37:32 +0300

    update

---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import CardStyle from "../scss/card.module.scss"
+
+const Card = ({data}) => {
+ 
+  return (
+    <div className={CardStyle.container}>
+      {data.map((item) =>{
+        const {img,comment,job,name}=item
+        return(
+          <div className={CardStyle.card}>
+            <h1>{name}</h1>
+            <h3>{job}</h3>
+            <p>{comment}</p>
+            <img src={img} alt={name} />
+            <div className={CardStyle.buttons}>
+            <button className={CardStyle.small}>Small</button>
+            <button className={CardStyle.large}>Large</button>
+            </div>
+           </div>
+           
+        )
+      })}
+     
+    </div>
+  )
+}
+
+export default Card
